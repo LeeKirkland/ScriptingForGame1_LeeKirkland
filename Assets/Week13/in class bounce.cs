ba148493b8f72ca93f15992cc40b8605013957fc
LeeKirkland <166537273+LeeKirkland@users.noreply.github.com>
2025-04-28 10:53:45 -0700

    in class notes

---
+++ Assets/Week13/in class bounce.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class inclassbounce : MonoBehaviour
+{
+    private Vector3 direction;
+    float speed
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "TopOrBottomWall")
+        {   
+        direction.y *= -1f;
+        }
+        else if (collision.collider.tag == "SideWall")
+        {
+        direction.x *= -1f;
+        }
+    }
+}
