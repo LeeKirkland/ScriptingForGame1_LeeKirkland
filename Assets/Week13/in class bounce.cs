afe88eba0781c407a0cad6f57a76eae8f6a039c0
LeeKirkland <166537273+LeeKirkland@users.noreply.github.com>
2025-04-28 10:55:40 -0700

    ;

--- Assets/Week13/in class bounce.cs
+++ Assets/Week13/in class bounce.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class inclassbounce : MonoBehaviour
 {
     private Vector3 direction;
-    float speed
+    float speed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
